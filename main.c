e9e97be7c9992580bd161fb084e7bae4178a1315
p6seok <p6seok@gmail.com>
2025-08-03 22:18:42 +0900

    offline pcap file analyzer

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <netinet/tcp.h> //TCP헤더 구조체를 위해 포함
 #include <string.h>
 #include <stdlib.h>
+#include <sys/time.h> //struct timeval을 위해 포함
+#include <stdint.h> // uint32_t를 위해 포함
 
 struct tcp_session {
     //ip
@@ -18,6 +20,16 @@ struct tcp_session {
     long data_transferred;
     double avg_rtt;
     int retransmissions;
+    struct timeval syn_time;
+    int handshake_state;
+
+    //재전송 탐지
+    uint32_t seen_seq_numbers[2048];
+    int seq_count;
+
+    //처리율 계산
+    struct timeval throughput_start_time; //처리율 측정 시작 시간
+    long bytes_in_interval; //측정 간격안에서의 누적바이트수
     
     struct tcp_session *next;// 다음세션 연결고리
 };
@@ -49,10 +61,15 @@ struct tcp_session* find_or_create_session(const char *src_ip, unsigned short sr
     new_session->data_transferred = 0;
     new_session->avg_rtt = 0.0;
     new_session->retransmissions = 0;
+    timerclear(&new_session->syn_time); //syn_time 0으로 초기화
+    new_session->handshake_state = 0; // 핸드셰이크 상태 초기화
+    new_session->seq_count = 0;
+    timerclear(&new_session->throughput_start_time);
+    new_session->bytes_in_interval = 0;
     new_session->next = session_list_head;
     session_list_head = new_session;
 
-    printf(">>> New Session Created: %s:%u <-> %s:%u\n", src_ip, src_port, dst_ip, dst_port);
+    printf(">>> 새로운 세션 생성 : %s:%u <-> %s:%u\n", src_ip, src_port, dst_ip, dst_port);
     return new_session;
 }
 
@@ -65,7 +82,8 @@ void packet_handler(u_char *args, const struct pcap_pkthdr *pkthdr, const u_char
     }
 
     struct tcphdr *tcp_header = (struct tcphdr *)(packet + 14 + ip_header_length);
-    
+    int tcp_header_length = tcp_header->th_off*4;
+
     //패킷 헤더에서 IP주소랑 포트번호 추출 및 변환
     char *src_ip_str = inet_ntoa(ip_header->ip_src);
     char *dst_ip_str = inet_ntoa(ip_header->ip_dst);
@@ -78,6 +96,80 @@ void packet_handler(u_char *args, const struct pcap_pkthdr *pkthdr, const u_char
     //세션 통계 정보 업데이트
     session->total_packets++;
     session->data_transferred += ntohs(ip_header->ip_len);
+
+    //RTT 계산 코드
+    // SYN 패킷 (client -> server)
+    if((tcp_header->th_flags & TH_SYN) && !(tcp_header->th_flags & TH_ACK)){
+        //최초 SYN이면
+        if (session->handshake_state == 0){
+        session->syn_time = pkthdr->ts;
+        session->handshake_state = 1; // SYN 받음
+        }
+    }
+
+    //SYNACK 패킷 (server->client)
+    else if ((tcp_header->th_flags & TH_SYN) && (tcp_header->th_flags & TH_ACK)){
+
+        if(session->handshake_state == 1){
+            long rtt_us = (pkthdr->ts.tv_sec - session->syn_time.tv_sec) * 1000000 +
+                          (pkthdr->ts.tv_usec - session->syn_time.tv_usec);
+
+            session->avg_rtt = (double)rtt_us /1000.0;
+
+            session->handshake_state = 2; //핸드셰이크 완료
+            printf("> RTT Calculated for %s:%u: %.2f ms\n", session->src_ip_str, session->src_port, session->avg_rtt);
+
+        }
+    }
+    //TCP 페이로드 길이 계산
+    int tcp_payload_len = ntohs(ip_header->ip_len) - ip_header_length - tcp_header_length;
+
+    if(tcp_payload_len > 0) {
+        //SEQ 번호를 네트워크 바이트 순서에서 호스트 순서로 변환
+        uint32_t current_seq = ntohl(tcp_header->th_seq);
+        int is_retransmission = 0;
+
+        // 이전에 저장되어있는 SEQ 번호인지 확인
+        for (int i = 0; i < session->seq_count; i++){
+            if(session->seen_seq_numbers[i] == current_seq){
+                is_retransmission = 1;
+                break;
+            }
+        }
+        if (is_retransmission){
+            //재전송이면 카운트증가
+            session->retransmissions++;
+            printf(">> 재전송 감지 %s:%u\n", session->src_ip_str, session->src_port);
+            }
+
+        else{
+            if(session->seq_count < 2048){
+                session->seen_seq_numbers[session->seq_count] = current_seq;
+                session->seq_count++;
+            }
+        }
+    }
+
+    //처리율 계산 코드
+    if(session->throughput_start_time.tv_sec == 0){
+        session->throughput_start_time = pkthdr->ts;
+    }
+    session->bytes_in_interval += ntohs(ip_header->ip_len);
+
+    long time_diff_us = (pkthdr->ts.tv_sec - session->throughput_start_time.tv_sec) * 1000000 + 
+    (pkthdr->ts.tv_usec - session->throughput_start_time.tv_usec);
+    
+                        
+    if(time_diff_us >=1000000){
+        double throughput_kbps = (double)session->bytes_in_interval * 8 / (time_diff_us / 1000000.0) / 1000.0;
+
+        printf(">>>>처리율 %s:%u: %.2f kbps\n", session->src_ip_str, session->src_port, throughput_kbps);
+
+        session->throughput_start_time = pkthdr->ts;
+        session->bytes_in_interval = 0;
+    }
+    
+
 }
 
 //요약파일 생성
@@ -139,3 +231,5 @@ int main() {
 
 // gcc main.c -o main -lpcap (컴파일)
 // ./main(실행)
+
+//Data Transferred, Avg RTT, Retransmissions 계산코드작성해야함
